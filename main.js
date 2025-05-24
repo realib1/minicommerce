fbfbd9f52f176aa2d66973ac21754075627ced4f
realib1 <ibeepversie@gmail.com>
2025-05-24 13:27:50 -0700

    Nebatect project phase 1, Mini eCommerce Website

---
+++ main.js
@@ -0,0 +1,21 @@
+class Person{
+    constructor(name, age, gender, isStudent) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.isStudent = isStudent;
+    }
+    
+    studentCheck() {
+        // this.isStudent ? 'Yes' : 'No';
+        console.log(`Name: ${this.name}\nGender: ${this.gender}\nStudent: ${this.isStudent ? 'Yes' : 'No'}`)
+
+    }
+}
+
+
+
+let student = new Person("John Doe", 18, 'Male', false)
+
+
+student.studentCheck()
